0d0a3fdc2472021747db61aa1aa9d1f43f918f86
Mark Chang <mark23456@hotmail.com>
2013-04-10 01:26:00 +0800

    final version

--- rasterize.h
+++ rasterize.h
@@ -92,7 +92,6 @@ public:
 	}
 	void print(){
 		//printf("%d\t%d\t%d\t\n",(int)_i[0],(int)_i[1],(int)_i[2]);
-
 		printf("%f\t%f\t%f\t\n",_p[0]->getx(1),_p[1]->getx(1),_p[2]->getx(1));
 	}
 	Vertex* getv(size_t i){
@@ -101,7 +100,6 @@ public:
 	size_t geti(size_t i){
 		return _i[i];
 	}
-
 	void swap(size_t i,size_t j){
 		Vertex* p=_p[i];
 		_p[i]=_p[j];
